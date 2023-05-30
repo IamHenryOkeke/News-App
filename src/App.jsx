bf21e6df4f1e85fcb0f82522c0316e49c4c6ac39
Henry Okeke <iamhenryokeke@gmail.com>
2023-05-31 00:40:39 +0100

    implemented custom fetch hook in app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,9 @@
-import { useState, useEffect } from "react"
 import Articles from "./components/Articles";
+import useFetch from "./hooks/useFetch";
 
 
 const App = () => {
-  const [loading, setLoading] = useState(true)
-  const [data, setData] = useState(null);
-  const [error, setError] = useState("")
-
-  const apiKey = import.meta.env.VITE_API_KEY;
-
-  const getData = () => {
-    fetch(`https://newsapi.org/v2/everything?q=tech&apiKey=${apiKey}`, {mode: 'cors'})
-    .then((response) =>{
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-      setData(data)
-      setLoading(false)
-      setError("")
-    }).catch((error) => {
-       console.log(error.message)
-       setError(`${error.message}`)
-       setLoading(false)
-    })
-    
-    
-  }
-
-  useEffect(() => {
-    getData();
-  }, [])
+  const {data, loading, error} = useFetch()
   
   return (
     <div className="flex flex-col items-center gap-10">
