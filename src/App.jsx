cb4b4b2cf769a79cef89ad820a4eaf09ea91dfda
Henry Okeke <iamhenryokeke@gmail.com>
2023-06-01 19:29:23 +0100

    imported filter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
+// import { useState } from "react";
 import Articles from "./components/Articles";
 import useFetch from "./hooks/useFetch";
+import Filter from "./components/Filter";
 
 
 const App = () => {
-  const {data, loading, error} = useFetch()
-  
+  const {data, loading, error} = useFetch();
   return (
-    <div className="flex flex-col items-center gap-10">
+    <div className="flex flex-col items-center gap-10 font-[Poppins]">
       <h1 className="text-6xl text-red-800">Breaking news</h1>
+      {
+        data && (
+          <Filter data={data.articles}/>        
+        )
+      }
       <div>
           {loading && (<p>
-            Loading .....
+            Loading.....
           </p>)}
           {error && (<p>
             {error}
